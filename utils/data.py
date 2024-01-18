9ebea210cb9de6fa90c7a5b8eb1da0ea142ad8ca
stefanvasilev <31345149+stefanvasilev@users.noreply.github.com>
2024-01-18 17:15:16 +0100

    added imagenette:320,160 options, image_feature_dim arg

--- utils/data.py
+++ utils/data.py
@@ -11,13 +11,13 @@ class ImagenetteDataset(object):
         if download:
           if not os.path.isdir(root):
             os.makedirs(root)
-            url = "https://s3.amazonaws.com/fast-ai-imageclas/imagenette2-320.tgz"
+            url = f"https://s3.amazonaws.com/fast-ai-imageclas/imagenette2-{patch_size}.tgz"
             filename = wget.download(url, out=root)
-            with tarfile.open(root+"/imagenette2-320.tgz", 'r:gz') as tar:
+            with tarfile.open(root+f"/imagenette2-{patch_size}.tgz", 'r:gz') as tar:
                 # Extract all contents to the specified directory
                 tar.extractall(path=root)
         
-        self.folder = Path(root+'/imagenette2-320/train') if not validation else Path(root+'/imagenette2-320/val')
+        self.folder = Path(root+f'/imagenette2-{patch_size}/train') if not validation else Path(root+f'/imagenette2-{patch_size}/val')
         self.classes = {'n01440764': "tench", 'n02102040': "English springer", 'n02979186': "cassette player", 
                         'n03000684': "chain saw", 'n03028079': "church", 'n03394916': "French horn", 
                         'n03417042': "garbage truck", 'n03425413': "gas pump", 
