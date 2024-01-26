Add option to fix softmax temperature, freeze CLIP model, add small value to A and B inside manifold loss for numerical stability

models/text_encoder.py
@@ -2,8 +2,8 @@ import os
 import torch
 import torch.nn as nn
 
-from clip import clip
-from clip.simple_tokenizer import SimpleTokenizer as _Tokenizer
+from .clip import clip
+from .clip.simple_tokenizer import SimpleTokenizer as _Tokenizer
 
 _tokenizer = _Tokenizer()
 

models/wideresnet_prompt.py
@@ -57,7 +57,7 @@ class NetworkBlock(nn.Module):
 
 
 class WideResNetPrompt(nn.Module):
-    def __init__(self, classnames, depth=28, widen_factor=2, drop_rate=0.0):
+    def __init__(self, classnames, depth=28, widen_factor=2, drop_rate=0.0, fixed_temperature=False):
         super(WideResNetPrompt, self).__init__()
         channels = [16, 16 * widen_factor, 32 * widen_factor, 64 * widen_factor]
         assert ((depth - 4) % 6 == 0)
@@ -94,7 +94,10 @@ class WideResNetPrompt(nn.Module):
 
         # This is a learned parameter, as noted just after Equation (1). We initialise it to log(1/0.07) as this is
         # the value used in the CLIP paper.
-        self.softmax_temp = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+        if not fixed_temperature:
+            self.softmax_temp = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+        else:
+            self.softmax_temp = torch.ones([]) * np.log(1 / 0.07)
 
         '''
         Prompt Learning
@@ -175,10 +178,11 @@ class WideResNetPrompt(nn.Module):
         return out
 
 class build_WideResNet:
-    def __init__(self, depth=28, widen_factor=2, drop_rate=0.0):
+    def __init__(self, depth=28, widen_factor=2, drop_rate=0.0, fixed_temperature=False):
         self.depth = depth
         self.widen_factor = widen_factor
         self.drop_rate = drop_rate
+        self.fixed_temperature=fixed_temperature
 
     def build(self, classnames):
         return WideResNetPrompt(
@@ -186,6 +190,7 @@ class build_WideResNet:
             classnames=classnames,
             widen_factor=self.widen_factor,
             drop_rate=self.drop_rate,
+            fixed_temperature=self.fixed_temperature
         )
 
 

train.py
@@ -1,11 +1,8 @@
-import torch
-import torch.nn as nn
 from torch.optim import SGD
 import torchvision
 import torchvision.transforms as transforms
 from models.wideresnet_prompt import *
 from models.modules.sinkhorn_distance import SinkhornDistance
-from torch.optim.lr_scheduler import CosineAnnealingLR
 from utils.misc import *
 import argparse
 import os
@@ -41,7 +38,6 @@ def train(net, trainloader, valloader, optimizer, scheduler, alpha, beta, w_dist
             # label_distribution: similarity matrix of the embedded prompts
             with torch.autocast(device_type='cuda', dtype=torch.float16, enabled=True):
                 out, AF, w_loss, AG = net(x, targets=y, w_distance=w_distance)
-
                 # cross-entropy loss
                 ce_loss = CELoss(out, y)
 
@@ -74,6 +70,7 @@ def train(net, trainloader, valloader, optimizer, scheduler, alpha, beta, w_dist
         writer.add_scalar('Average manifold loss', round(avg_m_loss, 3), epoch)
         writer.add_scalar('Average OT loss', round(avg_OT_loss, 3), epoch)
         writer.add_scalar('Average CE loss', round(avg_CE_loss, 3), epoch)
+        writer.add_scalar('Softmax temperature', round(net.softmax_temp.item(), 3), epoch)
 
         test_acc = get_accuracy(net, valloader, device)
         writer.add_scalar('Validation accuracy', round(test_acc, 3), epoch)
@@ -122,6 +119,8 @@ def main():
     parser.add_argument('--num_workers', type=int, default=8, help='Number of workers for dataloader')
     parser.add_argument('--seed', type=int, default=42, help='Seed for the random number generator')
     parser.add_argument('--weight_decay', type=float, default=0.0001, help='Weight decay')
+    parser.add_argument('--fixed_temperature', default=False, action='store_true',
+                        help="Whether to use a fixed softmax temperature")
 
     args = parser.parse_args()
 
@@ -235,7 +234,7 @@ def main():
                       'bicycle', 'bus', 'motorcycle', 'pickup truck', 'train',
                       'lawn-mower', 'rocket', 'streetcar', 'tank', 'tractor'])
 
-    wrn_builder = build_WideResNet(args.depth, args.width, 0.5)
+    wrn_builder = build_WideResNet(args.depth, args.width, 0.5, args.fixed_temperature)
     wrn = wrn_builder.build(classnames)
     wrn = wrn.to(device)
 
@@ -254,7 +253,13 @@ def main():
 
     w_distance = SinkhornDistance(args.sinkhorn_eps, args.sinkhorn_max_iters)
 
-    optimizer = SGD(wrn.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    # Freeze the clip model
+    for name, param in wrn.named_parameters():
+        if name.startswith('clip'):
+            param.requires_grad = False
+
+
+    optimizer = SGD(filter(lambda p: p.requires_grad, wrn.parameters()), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
     scheduler = CosineAnnealingStepLR(optimizer, T_max=num_steps)
 
     train(wrn, trainloader, valloader, optimizer, scheduler, args.alpha, args.beta, w_distance, num_epochs, device,

utils/misc.py
@@ -3,8 +3,9 @@ import torch.nn as nn
 from torch.optim.lr_scheduler import _LRScheduler
 import math
 
-def calculate_manifold_loss(A, B):
-    return torch.mean(torch.sum(A*torch.log(A/B), dim=1))
+def calculate_manifold_loss(A, B, eps=0.0001):
+    # Add small eps for numerical stability (avoid log(0) and log(inf)).
+    return torch.mean(torch.sum(A*torch.log((A + eps)/ (B+eps)), dim=1))
 
 
 def get_accuracy(model, test_loader, device):